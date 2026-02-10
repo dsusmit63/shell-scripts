da3bf78d96cc4ebbecf72d8f2bcff5f829c3fae9
dsusmit63 <dsusmit63@gmail.com>
2026-02-10 09:16:17 +0000

    added user-input.sh

---
+++ user-input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#this script takes your details as input and prints them
+
+read -p "Enter your fullname: " fullname
+
+read -p "Enter your role: " role
+
+echo "Hi $fullname.Good to hear that you are a $role."
+
+read -p "What is your years of experience?: " experience
+
+echo "Amazing!You have $experience years of experience."
