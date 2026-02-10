68e20bb1cf0c5af94ae3d5814f9b081d468b755b
dsusmit63 <dsusmit63@gmail.com>
2026-02-10 17:51:26 +0000

    added args-demo.sh

---
+++ args-demo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#this script prints total number of arguments, all the arguments, and the script name
+
+echo "Total number of arguments: $#"
+
+echo "All of the arguments are: $@"
+
+echo "The script name is: $0"
