b361de393fcb7f45df7f3d997e4f2b23fbab1441
dsusmit63 <dsusmit63@gmail.com>
2026-02-10 08:08:38 +0000

    added variable.sh

---
+++ variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#this script stores firstname,lastname and age as variable, and prints fullname along with age
+
+firstName="Susmit"
+
+lastName="Das"
+
+age=27
+
+echo "I am $firstName $lastName.I am $age years old."
