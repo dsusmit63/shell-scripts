8ecad1d8998853544608a36d2e6b2254a61c6250
dsusmit63 <dsusmit63@gmail.com>
2026-02-11 04:56:23 +0000

    added hello-fun.sh and hello-fun-import.sh

---
+++ hello-fun-import.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#this script imports a function from another file, and calls it
+
+source "./hello-fun.sh"
+
+hello-fun
+
---
+++ hello-fun.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#this script contains a function, that prints hello only, won't be called from this file, will be exported to another file
+
+hello-fun() {
+	echo "Hello...!"
+}
+
