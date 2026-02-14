d79dd494826ba9f48c18ac74ebc54b004998979c
dsusmit63 <dsusmit63@gmail.com>
2026-02-14 09:20:55 +0000

    modified addition.sh

--- addition.sh
+++ addition.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#this script takes two numbers as user input and prints their addition
+#this script takes two numbers as user input and prints their addition result
 
 read -p "Enter number1: " num1
 
 read -p "Enter number2: " num2
 
-result=$((num1 + num2))
+addResult=$((num1 + num2))
 
-echo "The addition of $num1 and $num2 is: $result"
+echo "The addition of $num1 and $num2 is: $addResult"
