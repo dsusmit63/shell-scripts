61f233ca4689ca118d5a65bb39c968f3a5895aa4
dsusmit63 <dsusmit63@gmail.com>
2026-02-14 10:01:16 +0000

    modified check-grade.sh

--- check-grade.sh
+++ check-grade.sh
@@ -2,19 +2,30 @@
 
 #this script takes marks as input and print grade based on it
 
-read -p "Enter marks: " marks
+readMarks(){
+	read -p "Enter marks: " marks
+        if [ "$marks" -lt 0 ] || [ "$marks" -gt 100 ]; then
+	             echo "Please enter valid marks: marks cannot be less than zero or more than 100"
+		     readMarks
+        fi
+}
 
-if [ "$marks" -gt 100 ]; then
-	echo "Enter valid marks"
-        exit 1
-fi
+checkGrade(){
+        if [ "$marks" -ge 90 ] && [ "$marks" -le 100 ]; then
+	               echo "Grade AA"
+        elif [ "$marks" -ge 75 ] && [ "$marks" -lt 90 ]; then
+	               echo "Grade A+"
+        elif [ "$marks" -ge 60 ] && [ "$marks" -lt 75 ]; then 
+                       echo "Grade A"
+        elif [ "$marks" -ge 40 ] && [ "$marks" -lt 60 ]; then
+	              echo "Grade B"
+        elif [ "$marks" -ge 30 ] && [ "$marks" -lt 40 ]; then
+	              echo "Grade C"
+        else
+	              echo "Grade D:Failed"
+        fi
+}
 
-if [ "$marks" -ge 90 ] && [ "$marks" -le 100 ]; then
-	echo "Grade A+"
-elif [ "$marks" -ge 60 ] && [ "$marks" -lt 90 ]; then 
-	echo "Grade A"
-elif [ "$marks" -ge 30 ] && [ "$marks" -lt 60 ]; then
-	echo "Grade B"
-else
-	echo "Grade C:Failed"
-fi
+readMarks
+
+checkGrade
