4d3bde4b3baf4f69ddb60716881412e347d98a17
dsusmit63 <dsusmit63@gmail.com>
2026-02-14 10:14:42 +0000

    modified check-voting-eligibility.sh

--- check-voting-eligibility.sh
+++ check-voting-eligibility.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-#this script takes user's age as input and print if user is eligible to vote or not
+#this script takes user's age and voter card availability as input and print if user is eligible to vote or not
 
 read -p "Enter your age: " age
-if [ "$age" -ge 18 ]; then
-	echo "you are eligible to vote"
+
+if [ "$age" -lt 18 ]; then
+	echo "You are under 18.Not eligible to vote"
+	exit 1
+fi
+
+read -p "Do you have voter card? (type y if yes, n for noi)" isAvailable
+
+if [ "$isAvailable" == "y" ]; then
+	echo "Congratulations!you are eligible to vote"
 else
-	echo "you are not eligible to vote"
+	echo "Hey!First apply for voter card"
 fi
 
